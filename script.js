e3fe133883603b162467c512303ea7648bb32531
Marcin Żebrowski <77941248+marcinzebrowski093@users.noreply.github.com>
2021-02-19 15:04:11 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navList = document.getElementsByClassName('nav-links')[0]
 const imageChange = document.getElementById('toggle-button')
+const headerbackground = document.getElementById('header')
 
 toggleButton.addEventListener('click', () => {
     navList.classList.toggle('active')
     imageChange.classList.toggle('active')
+    headerbackground.classList.toggle('active')
 })
